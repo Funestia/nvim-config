414ae0a3b7aca50321228243da013e5a232d6333
funestia <florian.russy6@gmail.com>
2024-08-21 16:09:40 +0200

    fix notify background color

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,7 +4,15 @@ return {
 	opts = {},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
+		{
+			"rcarriga/nvim-notify",
+			config = function ()
+				require("notify").setup {
+					background_colour = "#000000",
+				}
+			end,
+		lazy = false,
+		},
 		"hrsh7th/nvim-cmp",
 	},
 	config = function()
